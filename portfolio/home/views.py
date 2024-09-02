dbb43f6b9ad8751adee51e40113d24732192d65c
RafaelMatteo <rafaelmatteomourigan@gmail.com>
2024-09-02 11:28:05 -0300

    Delete unused imports

--- portfolio/home/views.py
+++ portfolio/home/views.py
@@ -1,10 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from django.conf import settings
 from twilio.rest import Client
-
-# Create your views here.
 
 # Twilio msg
 
